b91a4c06d0edaf48a57be4b92f704db3bc796dc0
Hamza Siddiqui <80615079+hamzapro305@users.noreply.github.com>
2023-12-11 02:20:16 +0500

    Authentication

--- backend-node/src/routes/testRouter.ts
+++ backend-node/src/routes/testRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import TestController from "../controllers/TestController";
 import { MulterConfigured } from "../config/Multer";
+import AuthMiddleware from "../Middlewares/AuthMiddleware";
 
 const TestRouter = Router()
 const upload = MulterConfigured([".zip", ".json"], "RAM")
 
-TestRouter.get("/", TestController.getTest)
-TestRouter.post("/upload", upload.single("file"), TestController.uploadFile)
+TestRouter.get("/", AuthMiddleware, TestController.getTest)
+TestRouter.post("/upload", AuthMiddleware, upload.single("file"), TestController.uploadFile)
 
 export default TestRouter
