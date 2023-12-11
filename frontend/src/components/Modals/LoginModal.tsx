f78486bfea216a1b1f24d16af8392272cd5475da
Yahya305 <saimyahya47@gmail.com>
2023-12-11 17:09:01 +0500

    upgraded framer motion to latest version

--- frontend/src/components/Modals/LoginModal.tsx
+++ frontend/src/components/Modals/LoginModal.tsx
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 import React from "react";
 import BackDrop from "../Backdrop";
 
 const LoginModal = () => {
-    return <BackDrop onClick={()=>console.log("bjkasbdjksdbjsabca") }>
-        <div>
-            
-        </div>
-    </BackDrop>;
+    return (
+        <BackDrop onClick={() => console.log("bjkasbdjksdbjsabca")}>
+            <div></div>
+        </BackDrop>
+    );
 };
 
 export default LoginModal;
