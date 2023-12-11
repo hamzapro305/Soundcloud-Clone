50e0cc209e664851dd72d91a7e95c6bc73570693
Yahya305 <saimyahya47@gmail.com>
2023-12-12 01:46:46 +0500

    worked on Profile Services

---
+++ backend-node/src/services/ProfileService.ts
@@ -0,0 +1,45 @@
+import { CustomError } from "../exceptions/CustomError";
+import HttpStatusCode from "../utils/HttpStatusCode";
+import profileRepository from "../repository/ProfileRepository";
+import { TProfile } from "../@Types/Profile";
+
+type UpdateAbleProfile = Partial<Pick<TProfile, "full_name" | "bio">>
+
+class ProfileService{
+    async createProfile(userId:string) : Promise<TProfile> {
+        try {
+            const new_profile = await profileRepository.createProfile(userId);
+            return new_profile;
+
+        } catch (error: any) {
+            throw new CustomError(
+                error?.message as string || 'Internal Server Error',
+                error?.httpCode || HttpStatusCode.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+    async updateProfile(uid: string, data: UpdateAbleProfile){
+        try {
+            const new_profile = await profileRepository.updateProfile(uid,data);
+            return new_profile;
+        } catch (error: any) {
+            throw new CustomError(
+                error?.message as string || 'Internal Server Error',
+                error?.httpCode || HttpStatusCode.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+    async getProfileByUID(uid: string){
+        try {
+            const new_profile = await profileRepository.getProfileByUID(uid);
+            return new_profile;
+        } catch (error: any) {
+            throw new CustomError(
+                error?.message as string || 'Internal Server Error',
+                error?.httpCode || HttpStatusCode.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
+
+export default ProfileService
