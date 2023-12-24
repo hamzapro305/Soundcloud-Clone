ab80502ee372e099ee1ecc2b4ad419898873d42d
Hamza Siddiqui <80615079+hamzapro305@users.noreply.github.com>
2023-12-24 13:58:23 +0500

    Google Repository

--- backend-node/src/controllers/UserController.ts
+++ backend-node/src/controllers/UserController.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import { NextFunction, Request, Response } from "express";
-import { injectable } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 import { UserServices } from "../services/UserServices";
 import ProfileService from "../services/ProfileService";
 import HttpStatusCode from "../utils/HttpStatusCode";
@@ -8,8 +8,11 @@ import HttpStatusCode from "../utils/HttpStatusCode";
 @injectable()
 export class UserController {
     constructor(
+        @inject(UserServices)
         private readonly _userService: UserServices,
-        private readonly _profileService: ProfileService
+
+        @inject(ProfileService)
+        private readonly _profileService: ProfileService,
     ) {}
 
     public readonly retrieveUser = async (
@@ -17,28 +20,32 @@ export class UserController {
         res: Response,
         next: NextFunction
     ) => {
-        const uid = req.params?.uid;
-        if (!uid) {
-            return res.status(HttpStatusCode.NOT_FOUND).json({
-                message: "User Id Invalid",
+        try {
+            const uid = req.params?.uid;
+            if (!uid) {
+                return res.status(HttpStatusCode.NOT_FOUND).json({
+                    message: "User Id Invalid",
+                });
+            }
+            let user = await this._userService.getByUID(uid);
+            if (!user) {
+                return res.status(HttpStatusCode.NOT_FOUND).json({
+                    message: "User not found",
+                });
+            }
+            let userProfile = await this._profileService.getProfileByUID(uid);
+            if (!userProfile) {
+                return res.status(HttpStatusCode.NOT_FOUND).json({
+                    message: "profile not found",
+                });
+            }
+            return res.status(HttpStatusCode.OK).json({
+                email: user?.email,
+                ...userProfile,
             });
+        } catch (error) {
+            next(error);
         }
-        let user = await this._userService.getByUID(uid);
-        if (!user) {
-            return res.status(HttpStatusCode.NOT_FOUND).json({
-                message: "User not found",
-            });
-        }
-        let userProfile = await this._profileService.getProfileByUID(uid);
-        if (!userProfile) {
-            return res.status(HttpStatusCode.NOT_FOUND).json({
-                message: "profile not found",
-            });
-        }
-        return res.status(HttpStatusCode.OK).json({
-            email: user?.email,
-            ...userProfile,
-        });
     };
 }
 
--- backend-node/src/repository/GoogleRepository.ts
+++ backend-node/src/repository/GoogleRepository.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
+import prisma from "../config/prisma-client";
+import { CustomError } from "../exceptions/CustomError";
+import HttpStatusCode from "../utils/HttpStatusCode";
+import { Provider } from "../@Types/Provider";
 
 export default class GoogleRepository {
-    readonly #GOOGLE_USERINFO_URL = `https://www.googleapis.com/oauth2/v2/userinfo`;
+    private readonly GOOGLE_USERINFO_URL = `https://www.googleapis.com/oauth2/v2/userinfo`;
 
     /**
      * Retrieves the Google profile information using the provided access token.
@@ -10,12 +14,45 @@ export default class GoogleRepository {
      */
     public readonly getGoogleProfile = async (accessToken: string) => {
         try {
-            const res = await axios.get(this.#GOOGLE_USERINFO_URL, {
+            const res = await axios.get(this.GOOGLE_USERINFO_URL, {
                 headers: {
                     Authorization: `Bearer ${accessToken}`,
                 },
             });
             return res.data;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    };
+
+    public readonly getGoogle = async (google_id: string) => {
+        try {
+            const google = await prisma.google.findUnique({
+                where: {
+                    google_id,
+                },
+            });
+            if (google == null) {
+                throw new CustomError(
+                    "Google Not Found for this id",
+                    HttpStatusCode.NOT_FOUND
+                );
+            }
+            return google as Provider["google"];
+        } catch (error: any) {
+            console.log(error);
+            return null;
+        }
+    };
+
+    public readonly getGoogleAccount = async (google_id: string) => {
+        try {
+            const google = await this.getGoogle(google_id);
+            const profile = await this.getGoogleProfile(
+                google?.access_token as string
+            );
+            return profile;
         } catch (error) {
             console.log(error);
             return null;
