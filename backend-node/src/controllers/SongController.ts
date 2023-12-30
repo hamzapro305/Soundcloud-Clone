e4c48ce67bf53add7ba1c52f4e98dad9bbe09230
Yahya305 <saimyahya47@gmail.com>
2023-12-30 19:57:59 +0500

    worked on songs service

--- backend-node/src/controllers/SongController.ts
+++ backend-node/src/controllers/SongController.ts
@@ -1,1 +1,21 @@
-class 
+import { inject } from "tsyringe";
+import SongService from "../services/SongService";
+
+export default class SongController {
+    constructor(
+        @inject(SongService)
+        private readonly _songService: SongService
+    ) {}
+    public readonly createSong = async (
+        req:Request, res:Response
+    ) => {
+        try {
+            
+            
+            const song = await this._songService.createSong("userID", data);
+            return song;
+        } catch (error) {
+            return null;
+        }
+    };
+}
