df1eb239c42bb852143e2e5534406e58c1a8f197
Hamza Siddiqui <80615079+hamzapro305@users.noreply.github.com>
2023-12-30 20:04:40 +0500

    Trying to fix router

--- backend-node/src/routes/SongRouter.ts
+++ backend-node/src/routes/SongRouter.ts
@@ -1,14 +1,19 @@
 import { Router } from "express";
 import { container } from "tsyringe";
 import Validation from "../Middlewares/Validation";
+import AuthMiddleware from "../passport/AuthMiddleware";
 
 const SongRouter = Router()
 
 const validator = container.resolve(Validation)
+const authMiddleware = container.resolve(AuthMiddleware)
+const songController = container.resolve(SongController)
 
 SongRouter.post(
     "/",
-    validator.createSongValidator
+    authMiddleware.isLoggedIn,
+    validator.createSongValidator,
+
 )
 
 export default SongRouter;
