92d53e97a993c2a7baf188cf93f4f375dafb07c7
Yahya305 <saimyahya47@gmail.com>
2024-01-05 01:38:43 +0500

    worked on comment repo

--- backend-node/src/repository/CommentRepository.ts
+++ backend-node/src/repository/CommentRepository.ts
@@ -25,6 +25,7 @@ class CommentRepository {
             return null;
         }
     };
+    
 
 }
 
--- backend-node/src/services/CommentServices.ts
+++ backend-node/src/services/CommentServices.ts
@@ -1,5 +1,65 @@
-export default class CommentServices {
-    constructor() {
-        
-    }
+import { inject, singleton } from "tsyringe";
+import CommentRepository from "../repository/CommentRepository";
+import SongRepository from "../repository/SongRepository";
+import { EmptySong } from "../@Types/Song";
+import ProfileService from "./ProfileService";
+import SongService from "./SongService";
+import { CustomError } from "../exceptions/CustomError";
+import HttpStatusCode from "../utils/HttpStatusCode";
+import UploadService from "./UploadService";
+
+@singleton()
+class CommentService {
+    constructor(
+        @inject(CommentRepository)
+        private readonly _commentRepository: CommentRepository,
+
+        @inject(ProfileService)
+        private readonly _profileService: ProfileService,
+
+        @inject(SongRepository)
+        private readonly _songRepository: SongRepository
+    ) {}
+
+    public readonly postComment = async (
+        profile_id: string,
+        song_id: string,
+        content: string,
+        timestamp:number
+    ) => {
+        try {
+            const profile = await this._profileService.getProfileByUID(profile_id);
+            await this._songRepository.getSong(song_id);
+            const comment = await this._commentRepository.createComment(
+                profile_id,
+                song_id,
+                content,
+                timestamp
+            );
+            return comment;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    };
+
+    public readonly updateComment = async (
+        songID: string,
+        data: Partial<EmptySong>
+    ) => {
+        try {
+            const updatedSong = await this._commentRepository.updateSong(
+                songID,
+                data
+            );
+            return updatedSong;
+        } catch (error) {
+            throw new CustomError(
+                "Internal Server Error",
+                HttpStatusCode.INTERNAL_SERVER_ERROR
+            );
+        }
+    };
 }
+
+export default CommentService;
